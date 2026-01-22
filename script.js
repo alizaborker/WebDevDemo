a327614f4c5d887ee9c7dcc8e17bfce018a069f7
alizaborker <57578671+alizaborker@users.noreply.github.com>
2026-01-22 09:14:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let score = 0;
+let timeLeft = 30;
+
+// talk about how we defines consts to refer to HTML elements
+const scoreDisplay = document.getElementById("score");
+const timeDisplay = document.getElementById("time");
+const gameArea = document.getElementById("game-area");
+
+function spawnGhost() {
+  const ghost = document.createElement("img");
+  ghost.src = "https://www.pngarts.com/files/18/Ghost-PNG-HQ-Picture.png"; // ghost pic
+  // add ghost to CSS class 'ghost' to apply effects
+  ghost.classList.add("ghost");
+
+  // random position in game area
+  ghost.style.left = Math.random() * (gameArea.clientWidth - 80) + "px";
+  ghost.style.top = Math.random() * (gameArea.clientHeight - 80) + "px";
+
+  // makes ghost visible in game area
+  gameArea.appendChild(ghost);
+
+  // click event
+  ghost.addEventListener("click", () => {
+    score++;
+    scoreDisplay.textContent = score;
+    ghost.remove();
+  });
+
+  // ghost disappears after 1.2 seconds
+  setTimeout(() => ghost.remove(), 1200);
+}
+
+// 30 sec timer
+function countdown() {
+  const timer = setInterval(() => {
+    timeLeft--;
+    timeDisplay.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      clearInterval(ghostSpawner);
+      alert("Time’s up! Your final score is " + score + ".");
+    }
+  }, 1000);
+}
+
+// spawn new ghost every 800ms
+const ghostSpawner = setInterval(spawnGhost, 800);
+countdown();
